571fed8c4695d383fc07fed07cc650c8b3ed1d4e
joemeister52 <bobmonkey1997@hotmail.com>
2023-03-05 13:53:51 -0800

    Created test branch and changed color test

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     // <View style={styles.container}>
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-blue-900">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
