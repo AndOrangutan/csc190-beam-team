773fc57cd8b936a49cf0942c3b2c5482c128b13c
Von Mueller <vonjonmueller@gmail.com>
2023-04-02 10:46:56 -0700

    Configured tailwind for file structure, works now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
+    './App.{js,jsx,ts,tsx}',
+    './Screens/**/*.{js,jsx,ts,tsx}',
+    './Navigation/**/*.{js,jsx,ts,tsx}',
     // Example
     // "./<custom directory>/**/*.{js,jsx,ts,tsx}"
   ],
@@ -9,4 +11,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
