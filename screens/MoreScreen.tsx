a3f9d43847b2dd4e2e19bc84c702ffc65035478e
Joemeister52 <bobmonkey1997@hotmail.com>
2023-04-04 11:56:52 -0700

    91 wrapped text in View to stylize

--- screens/MoreScreen.tsx
+++ screens/MoreScreen.tsx
@@ -3,7 +3,7 @@ import { View, Text, SafeAreaView } from 'react-native';
 
 const MoreScreen = () => {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-teal-300">
+    <SafeAreaView className="flex-1 items-center justify-center ">
       <Text>MoreScreen</Text>
     </SafeAreaView>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,jsx,ts,tsx}"
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./navigator/**/*.{tsx}",
     // Example
     // "./<custom directory>/**/*.{js,jsx,ts,tsx}"
   ],
