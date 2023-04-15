b8e370383dc7a55a6035c046afe82c0ab3504185
Joemeister52 <bobmonkey1997@hotmail.com>
2023-04-15 14:05:52 -0700

    87 88 103 120 tasks done Login page made
    Login page has dummy login process and has the option to browse as guest
    register does nothing at the moment. text boxes move with keyboard on ios
    need testing with android

--- App.tsx
+++ App.tsx
@@ -1,30 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './navigator/RootNavigator';
-
-import HomeScreen from './screens/HomeScreen';
+import React from 'react';
 
 export default function App() {
   return (
-    // Default
-    // <View style={styles.container}>
-    // TailwindCSS
-    // <View className="flex-1 items-center justify-center bg-white">
-    //   <Text>Open up App.tsx to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
     "./screens/**/*.{js,jsx,ts,tsx}",
-    "./navigator/**/*.{tsx}",
+    "./navigator/**/*.tsx ",
     // Example
     // "./<custom directory>/**/*.{js,jsx,ts,tsx}"
   ],
