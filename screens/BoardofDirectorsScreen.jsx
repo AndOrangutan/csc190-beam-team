cae433718fc691c88bc0376f9cabe8d301e1cc0a
tedkim <tedmkim2000@gmail.com>
2023-09-16 11:49:48 -0700

    Board of Directors Template

--- screens/BoardofDirectorsScreen.jsx
+++ screens/BoardofDirectorsScreen.jsx
@@ -1,12 +1,191 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const BoardofDirectorsScreen = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-teal-300">
-        <Text>Board of Directors</Text>
-        <Text>Coming soon...</Text>
-    </View>
+    <SafeAreaView className="h-full items-center justify-center">
+      <ScrollView className="w-10/12">
+        {/* EC Title */}
+        <View>
+          <Text className="font-bold text-center text-black text-2xl p-2">Executive Committee</Text>
+        </View>
+
+        {/* EC Tiles */}
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        {/* BOD Title */}
+        <View>
+          <Text className="font-bold text-center text-black text-2xl p-2">Board of Directors</Text>
+        </View>
+
+        {/* BOD Tiles */}
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+
+        <View className="items-center justify-evenly bg-cyan-300 w-600 p-4">
+          <Text className="font-bold text-white text-2xl mb-4">
+            Name
+          </Text>
+          <Text className="text-center text-black text-lg mb-4">
+            Position
+          </Text>
+        </View>
+
+        <View className="items-center justify-evenly p-4"></View>
+      </ScrollView>
+    </SafeAreaView>
+    
   )
 }
 
