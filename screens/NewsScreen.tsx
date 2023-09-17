fff3f2b32d756072452c524dc8cdc871700dbffb
mark-csus <mburgasser@csus.edu>
2023-09-17 14:18:01 -0700

    Adding a News screen to the "More" screen

---
+++ screens/NewsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native';
+
+const NewsScreen = () => {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-teal-300">
+      <Text>NewsScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default NewsScreen
