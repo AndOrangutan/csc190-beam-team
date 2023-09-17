e7863b5ba84940607a59305f8bdebbb9159d3275
Yingyan <ydeng@csus.edu>
2023-09-16 23:47:59 -0700

    add a basic map on map screen

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,24 +1,35 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useLayoutEffect } from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 // https://reactnavigation.org/docs/tab-based-navigation
 // https://oblador.github.io/react-native-vector-icons/
 
 const MapScreen = () => {
-  const navigation = useNavigation();
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerShown: false, // disable "Main" header
-    });
-  }, );
+  const [mapRegion, setmapRegion]=useState({
+    latitude: 38.59184,
+    longitude: -121.33438,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
 
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-teal-300">
-      <Text>MapScreen</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <MapView
+        style={{ alignSelf: 'stretch', height: '100%' }}
+        region={mapRegion}
+      >
+        <Marker coordinate={mapRegion} title='American River Parkway Foundation' />
+      </MapView>
+    </View>
   );
 };
+
+const styles=StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default MapScreen;
