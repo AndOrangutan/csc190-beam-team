79b30ff7127bb3ccdddb0838db755246066c406b
mark-csus <mburgasser@csus.edu>
2023-09-17 14:29:20 -0700

    BT-116 ARPF News

--- screens/NewsScreen.tsx
+++ screens/NewsScreen.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import WebView from 'react-native-webview';
 
 const NewsScreen = () => {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-teal-300">
-      <Text>NewsScreen</Text>
-    </SafeAreaView>
-  )
-}
+    <WebView
+      source={{
+        uri: 'https://arpf.org/coverage/',
+      }}
+      style={{ marginTop: -700 }}
+    />
+  );
+};
 
-export default NewsScreen
+export default NewsScreen;
