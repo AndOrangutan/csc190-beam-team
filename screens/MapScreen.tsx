5912038873e6cb591a13474be47697ea5d7cbc22
mark-csus <mburgasser@csus.edu>
2023-09-28 01:35:47 -0700

    Formatting and commenting

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 //import { GOOGLE_API_KEY } from 'react-native-dotenv'
+import { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 const locations = [
   {
@@ -15,167 +16,166 @@ const locations = [
   {
     name: 'Discovery Park Restroom',
     coordinate: {
-      latitude: 38.60394376641583, 
-      longitude:-121.50299408320325
+      latitude: 38.60394376641583,
+      longitude: -121.50299408320325,
     },
   },
   {
     name: 'William B. Pond Public Restroom',
     coordinate: {
-    latitude: 38.58425483312594, 
-    longitude: -121.33429994660013
+      latitude: 38.58425483312594,
+      longitude: -121.33429994660013,
     },
   },
 
   {
     name: 'William B. Pond Restroom',
     coordinate: {
-    latitude: 38.58745300401968, 
-    longitude: -121.33485753447236
+      latitude: 38.58745300401968,
+      longitude: -121.33485753447236,
     },
   },
   {
     name: 'River Bend Park Restroom',
     coordinate: {
-    latitude: 38.594985909962936, 
-    longitude: -121.32974181610682
+      latitude: 38.594985909962936,
+      longitude: -121.32974181610682,
     },
   },
   {
     name: 'Discovery Park Parking',
     coordinate: {
       latitude: 38.60540636296965,
-      longitude: -121.50292273964769
+      longitude: -121.50292273964769,
     },
   },
   {
     name: 'Bushy Lake Parking',
     coordinate: {
-      latitude: 38.586616210289236, 
-      longitude: -121.42084320855987
+      latitude: 38.586616210289236,
+      longitude: -121.42084320855987,
     },
   },
   {
     name: 'Watt Access Parking',
     coordinate: {
-      latitude: 38.5661587143369, 
-      longitude: -121.38227412442873
+      latitude: 38.5661587143369,
+      longitude: -121.38227412442873,
     },
   },
   {
     name: 'William B. Pond Recreational Area Parking',
     coordinate: {
-      latitude: 38.585142588191495, 
-      longitude: -121.33484131863034
+      latitude: 38.585142588191495,
+      longitude: -121.33484131863034,
     },
   },
   {
     name: 'River Bend Parking',
     coordinate: {
-    latitude: 38.59592281831733, 
-    longitude: -121.32984458321144
+      latitude: 38.59592281831733,
+      longitude: -121.32984458321144,
     },
   },
   {
     name: 'Ancil Hoffman Parking B',
     coordinate: {
-    latitude: 38.61254780632622, 
-    longitude: -121.30798593958201
+      latitude: 38.61254780632622,
+      longitude: -121.30798593958201,
     },
   },
   {
     name: 'Ancil Hoffman Parking A',
     coordinate: {
-    latitude: 38.61067560203675, 
-    longitude: -121.30875623583161
+      latitude: 38.61067560203675,
+      longitude: -121.30875623583161,
     },
   },
   {
     name: 'Rossmoor Bar Parking',
     coordinate: {
-    latitude: 38.62432550882811, 
-    longitude: -121.3019233231389
+      latitude: 38.62432550882811,
+      longitude: -121.3019233231389,
     },
   },
   {
     name: 'Jim Jones Bridge (PCA) Parking',
     coordinate: {
-    latitude: 38.6317441799987, 
-    longitude: -121.26998596185686
+      latitude: 38.6317441799987,
+      longitude: -121.26998596185686,
     },
   },
   {
     name: 'South Bridge Street Parking',
     coordinate: {
-    latitude: 38.63491542560191, 
-    longitude: -121.26327193749412
+      latitude: 38.63491542560191,
+      longitude: -121.26327193749412,
     },
   },
   {
     name: 'Sailor Bar Park Parking A',
     coordinate: {
-    latitude: 38.64044969402223, 
-    longitude: -121.25087657317405
+      latitude: 38.64044969402223,
+      longitude: -121.25087657317405,
     },
   },
   {
     name: 'Sailor Bar Park Parking B',
     coordinate: {
-    latitude: 38.637856834209195, 
-    longitude: -121.23890510020661
+      latitude: 38.637856834209195,
+      longitude: -121.23890510020661,
     },
   },
   {
     name: 'Hazel Ave South Parking',
     coordinate: {
-    latitude: 38.63479417078463, 
-    longitude: -121.22228242198304
+      latitude: 38.63479417078463,
+      longitude: -121.22228242198304,
     },
   },
   {
     name: 'Hazel Ave North Parking',
     coordinate: {
-    latitude: 38.63761757207717, 
-    longitude: -121.22215756786571
+      latitude: 38.63761757207717,
+      longitude: -121.22215756786571,
     },
   },
   {
     name: 'Mississippi Bar Parking',
     coordinate: {
-    latitude: 38.655414984121144, 
-    longitude: -121.20436084784136
+      latitude: 38.655414984121144,
+      longitude: -121.20436084784136,
     },
   },
   {
     name: 'Black Miners Bar Parking',
     coordinate: {
-    latitude: 38.68209325086604, 
-    longitude: -121.18461885355966
+      latitude: 38.68209325086604,
+      longitude: -121.18461885355966,
     },
   },
   {
     name: 'Willow Creek Recreation Area Parking',
     coordinate: {
-    latitude: 38.648322469160334, 
-    longitude: -121.19010161350329
+      latitude: 38.648322469160334,
+      longitude: -121.19010161350329,
     },
   },
   {
     name: 'Folsom Dam Parking South',
     coordinate: {
-    latitude: 38.705639342784785, 
-    longitude: -121.15615296394265 
+      latitude: 38.705639342784785,
+      longitude: -121.15615296394265,
     },
   },
   {
     name: 'Folsom Dam Parking North',
     coordinate: {
-    latitude: 38.70958442812909, 
-    longitude: -121.16690745469171
+      latitude: 38.70958442812909,
+      longitude: -121.16690745469171,
     },
   },
   // Add more locations here
-
 ];
 
 const MapScreen = () => {
@@ -189,7 +189,7 @@ const MapScreen = () => {
   return (
     <View style={styles.container}>
       <GooglePlacesAutocomplete
-        placeholder='Search'
+        placeholder="Search"
         onPress={(data, details = null) => {
           // 'details' is provided when fetchDetails = true
           console.log(data, details);
@@ -205,11 +205,7 @@ const MapScreen = () => {
         style={{ alignSelf: 'stretch', height: '100%' }}
         region={mapRegion}>
         {locations.map((location, index) => (
-          <Marker
-            key={index}
-            coordinate={location.coordinate}
-            title={location.name}
-          />
+          <Marker key={index} coordinate={location.coordinate} title={location.name} />
         ))}
       </MapView>
     </View>
