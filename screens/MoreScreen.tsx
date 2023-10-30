fde969ed6665f894cacea5c35703fe31c9894b28
sahdkhan <03sahd@gmail.com>
2023-10-29 18:04:57 -0700

    Fixed some merge problems, added login/logout button again, and fixed up some styling issues

--- screens/MoreScreen.tsx
+++ screens/MoreScreen.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
 import * as SecureStore from 'expo-secure-store';
 import React, { useEffect } from 'react';
+import { View, Text, Image, Linking, ImageBackground } from 'react-native';
 import { List } from 'react-native-paper';
 import Animated, {
   useAnimatedStyle,
@@ -8,7 +9,6 @@ import Animated, {
   withTiming,
   withSpring,
 } from 'react-native-reanimated';
-import { View, Text, Image, Linking } from 'react-native';
 
 const MoreScreen = ({ logout }: any) => {
   const [user, setUser] = React.useState<any>(null);
@@ -41,94 +41,20 @@ const MoreScreen = ({ logout }: any) => {
         className="p-6 shadow-md bg-zinc-50"
         title="Donate Now"
         left={(props) => <List.Icon {...props} icon="cash" />}
-        onPress={() => Linking.openURL("https://arpf.org/donation/")}
+        onPress={() => Linking.openURL('https://arpf.org/donation/')}
       />
       <List.Item
         className="p-6 shadow-md bg-zinc-50"
         title="Park Passes"
         left={(props) => <List.Icon {...props} icon="parking" />}
-        onPress={() => Linking.openURL("https://shop.arpf.org/?product_cat=parks-passes")}
+        onPress={() => Linking.openURL('https://shop.arpf.org/?product_cat=parks-passes')}
       />
       <List.Item
         className="p-6 shadow-md bg-zinc-50"
         title="Contact Us"
         left={(props) => <List.Icon {...props} icon="phone" />}
-        onPress={() => Linking.openURL("https://arpf.org/contact-us/")}
-        
+        onPress={() => Linking.openURL('https://arpf.org/contact-us/')}
       />
-      {/* Image component */}
-      <Image
-        source={require('../assets/Sunset.jpg')}
-        style={{ width: '100%', height: 300, resizeMode: 'cover' }}
-      />
-
-<View style={{position: 'absolute',
-    top: 270,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    opacity:.6 ,
-    height:250,
-    backgroundColor:'white'}}/>
-
-
-      <Text style={{ position: 'absolute',
-    top: 280,
-    left: 140,
-    fontSize: 24,
-    fontWeight: 'bold',}}>
-        MISSION
-      </Text>
-      <Text style={{ textAlign: 'center', paddingBottom: 220, fontSize: 20, position:'absolute', bottom: 0 }}>
-        The American River Parkway Foundation (APRF) leads and inspires the community to conserve and nurture the American River Parkway as a unique, accessible resource for everyone to enjoy.
-      </Text>
-    </View>
-
-      {/* Viste the Parkway dropdown list */}
-      <List.Accordion style={{ padding: 30 }} title="Visit the parkway">
-        <List.Item
-          className="p-6 shadow-md bg-zinc-50"
-          title="Parks"
-          left={(props) => <List.Icon {...props} icon="flower" />}
-          onPress={() => {
-            navigation.navigate('parks');
-          }}
-        />
-        <List.Item
-          className="p-6 shadow-md bg-zinc-50"
-          title="Park Passes"
-          left={(props) => <List.Icon {...props} icon="parking" />}
-          onPress={() => {
-            navigation.navigate('ParkPass');
-          }}
-        />
-        <List.Item
-          className="p-6 shadow-md bg-zinc-50"
-          title="Interactive Map"
-          left={(props) => <List.Icon {...props} icon="map-marker" />}
-          onPress={() => {
-            navigation.navigate('Interactive Map');
-          }}
-        />
-        <List.Item
-          className="p-6 shadow-md bg-zinc-50"
-          title="We Love the Parkway"
-          left={(props) => <List.Icon {...props} icon="heart" />}
-          onPress={() => {
-            navigation.navigate('We Love The ParkWay');
-          }}
-        />
-        <List.Item
-          className="p-6 shadow-md bg-zinc-50"
-          title="Face of the  Parkway"
-          left={(props) => <List.Icon {...props} icon="tag-faces" />}
-          onPress={() => {
-            navigation.navigate('Face Of The ParkWay');
-          }}
-        />
-      </List.Accordion>
-
-      {/* Logout button if user is signed in and Login button if not*/}
       {user ? (
         <List.Item
           className="p-6 shadow-md bg-zinc-50"
@@ -144,8 +70,64 @@ const MoreScreen = ({ logout }: any) => {
           onPress={() => logout()}
         />
       )}
-    </Animated.ScrollView>
 
+      {/* Mission Statement */}
+      <View className="flex-1">
+        <ImageBackground
+          source={require('../assets/Sunset.jpg')}
+          resizeMode="cover"
+          className="flex-1 justify-center items-center ">
+          <View
+            className="absolute top-0 left-0 bottom-0 right-0"
+            style={{ backgroundColor: 'rgba(0,0,0,0.3)' }}
+          />
+          <Text className="font-bold text-white uppercase text-2xl">Mission</Text>
+          <Text className="text-white shadow-lg shadow-black text-lg text-center p-4">
+            The American River Parkway Foundation (APRF) leads and inspires the community to
+            conserve and nurture the American River Parkway as a unique, accessible resource for
+            everyone to enjoy.
+          </Text>
+        </ImageBackground>
+      </View>
+
+      {/* Image component
+      <Image
+        source={require('../assets/Sunset.jpg')}
+        style={{ width: '100%', height: 300, resizeMode: 'cover' }}
+      />
+
+      <View
+        style={{
+          position: 'absolute',
+          top: 360,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          opacity: 0.6,
+          height: 250,
+          backgroundColor: 'white',
+        }}
+      >
+
+      <Text style={{ position: 'absolute', top: 370, left: 140, fontSize: 24, fontWeight: 'bold' }}>
+        MISSION
+      </Text>
+      <Text
+        style={{
+          textAlign: 'center',
+          paddingBottom: 220,
+          fontSize: 20,
+          position: 'absolute',
+          bottom: 0,
+        }}>
+        The American River Parkway Foundation (APRF) leads and inspires the community to conserve
+        and nurture the American River Parkway as a unique, accessible resource for everyone to
+        enjoy.
+      </Text>
+      </View> */}
+    </View>
+
+    /* Logout button if user is signed in and Login button if not*/
   );
 };
 
