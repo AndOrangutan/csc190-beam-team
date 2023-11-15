8b8d46e462b16d81b2433230792e0d10fc46c6e6
Von Mueller <vonjonmueller@gmail.com>
2023-11-15 00:38:20 -0800

    Testing tests for test reasons

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './navigator/RootNavigator';
 import 'react-native-gesture-handler';
 
+// testing tests for testing reasons
+
 export default function App() {
   return (
     <SafeAreaProvider>
