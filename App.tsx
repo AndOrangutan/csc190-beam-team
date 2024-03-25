2054af5ea61e2e057a7c8c03838491f9976279eb
Von Mueller <vonjonmueller@gmail.com>
2024-03-25 00:53:44 -0700

    Fix startup errors Techincaly we are just suppressing them, but they are due to out of date Dependencies.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { LogBox } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import RootNavigator from './navigator/RootNavigator';
 import 'react-native-gesture-handler';
+
+LogBox.ignoreLogs(['ViewPropTypes will be removed', 'ColorPropType will be removed']);
 
 export default function App() {
   return (
